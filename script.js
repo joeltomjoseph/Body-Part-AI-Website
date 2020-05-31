8e824fb63d5539a6792c9ad8c54d4c59682969b0
probablynotjoel <joeltom1408@gmail.com>
2020-05-31 12:15:36 +0100

    Fixed CSS and Capitalized the ol, now try sort it.

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ async function init() {
     document.getElementById("webcam-container").appendChild(webcam.canvas);
     labelContainer = document.getElementById("label-container");
     for (let i = 0; i < maxPredictions; i++) { // and class labels
-        labelContainer.appendChild(document.createElement("div"));
+        labelContainer.appendChild(document.createElement("li"));
     }
 }
 async function loop() {
